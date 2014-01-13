67da7e401719269c441797586db8bed7cfaa8abd
Benoît Hamel <b.hamel27@gmail.com>
2014-01-13 10:48:12 -0500

    upload1

---
+++ tp2-def.scm
@@ -0,0 +1,402 @@
+;; Fichier : "tp2-def.scm"
+
+;; Copyright (C) 2001-2012 Universite de Montreal, Tous Droits Reserves.
+
+;;-----------------------------------------------------------------------------
+
+;; Definitions pour le TP2.
+
+;;-----------------------------------------------------------------------------
+
+;; Representation des vecteurs et segments.
+
+(define vect cons)
+(define vect-x car)
+(define vect-y cdr)
+
+(define segm (lambda (depart arrivee) (list depart arrivee)))
+(define segm-depart car)
+(define segm-arrivee cadr)
+
+;;-----------------------------------------------------------------------------
+
+;; Parcours normalises pour les chiffres 0 a 9.
+
+(define parcours-pour-chiffres
+  (list->vector
+    (list
+
+      (list (vect 0 7/8)        ; 0
+            (vect 2/3 1/2)
+            (vect 2/3 -1/2)
+            (vect 0 -7/8)
+            (vect -2/3 -1/2)
+            (vect -2/3 1/2)
+            (vect 0 7/8))
+
+      (list (vect -1/4 3/4)     ; 1
+            (vect 0 7/8)
+            (vect 0 -7/8))
+
+      (list (vect -2/3 1/2)     ; 2
+            (vect 0 7/8)
+            (vect 2/3 1/2)
+            (vect -2/3 -7/8)
+            (vect 2/3 -7/8))
+
+      (list (vect -2/3 1/2)     ; 3
+            (vect 0 7/8)
+            (vect 2/3 1/2)
+            (vect 0 0)
+            (vect 2/3 -1/2)
+            (vect 0 -7/8)
+            (vect -2/3 -1/2))
+
+      (list (vect 1/2 0)        ; 4
+            (vect -1/2 0)
+            (vect 1/2 7/8)
+            (vect 1/2 -7/8))
+
+      (list (vect 2/3 7/8)      ; 5
+            (vect -2/3 7/8)
+            (vect -2/3 1/2)
+            (vect 2/3 0)
+            (vect 2/3 -1/2)
+            (vect 0 -7/8)
+            (vect -2/3 -1/2))
+
+      (list (vect 2/3 7/8)      ; 6
+            (vect 0 7/8)
+            (vect -2/3 1/2)
+            (vect -2/3 -1/2)
+            (vect 0 -7/8)
+            (vect 2/3 -1/2)
+            (vect 0 0)
+            (vect -2/3 0))
+
+      (list (vect -2/3 7/8)     ; 7
+            (vect 2/3 7/8)
+            (vect 0 0)
+            (vect 0 -7/8))
+
+      (list (vect 0 7/8)        ; 8
+            (vect 2/3 1/2)
+            (vect -2/3 -1/2)
+            (vect 0 -7/8)
+            (vect 2/3 -1/2)
+            (vect -2/3 1/2)
+            (vect 0 7/8))
+
+      (list (vect 2/3 0)        ; 9
+            (vect 0 0)
+            (vect -2/3 1/2)
+            (vect 0 7/8)
+            (vect 2/3 1/2)
+            (vect 2/3 -1/2)
+            (vect 0 -7/8)
+            (vect -2/3 -1/2))
+    )))
+
+;;-----------------------------------------------------------------------------
+
+;; Fonctions utilitaires.
+
+(define foldr
+  (lambda (f base lst)
+    (if (null? lst)
+        base
+        (f (car lst) (foldr f base (cdr lst))))))
+
+(define foldl
+  (lambda (f base lst)
+    (if (null? lst)
+        base
+        (foldl f (f base (car lst)) (cdr lst)))))
+
+;;-----------------------------------------------------------------------------
+
+;; Visualisation d'un dessin.
+
+(define dessiner
+  (lambda (dessinateur)
+    (afficher (dessinateur (lambda (v) v)))))
+
+(define afficher
+  (lambda (lst)
+
+    (for-each
+     (lambda (s)
+       (if (not (and (pair? s) (pair? (cdr s)) (null? (cddr s))
+                     (pair? (segm-depart s))
+                     (pair? (segm-arrivee s))
+                     (number? (vect-x (segm-depart s)))
+                     (number? (vect-y (segm-depart s)))
+                     (number? (vect-x (segm-arrivee s)))
+                     (number? (vect-y (segm-arrivee s)))))
+           (error "le parametre de la fonction \"afficher\" n'est pas valide")))
+     lst)
+
+    (set! segments lst)))
+
+(define segments '())
+
+;;-----------------------------------------------------------------------------
+
+;; Interface a Tcl/Tk.
+
+(define tk-program "tclsh")
+(define canv-width 600)
+(define canv-height 600)
+(define grid-color "pink")
+
+(define-type tkproc
+  port
+  sync-port
+  handle
+  sizes
+  segments
+)
+
+(define tk-open
+  (lambda (handle)
+    (let ((p (open-process
+              (list (string->keyword "path")
+                    tk-program
+                    (string->keyword "stderr-redirection")
+                    #t))))
+      (let ((tkproc (make-tkproc p (open-vector) handle #f #f)))
+        (if (##unbound? (##global-var-ref (##make-global-var 'tk-tkproc)))
+            (eval '(define tk-tkproc #f))
+            (tk-terminate tk-tkproc))
+        (set! tk-tkproc tkproc)
+        (tk-start-event-loop tkproc)
+        (##add-exit-job!
+         (lambda ()
+           (tk-terminate tkproc)))
+        tkproc))))
+
+(define tk-terminate
+  (lambda (tkproc)
+    (tk-cmd tkproc "exit")
+    (tk-close tkproc)))
+
+(define tk-close
+  (lambda (tkproc)
+    (let ((port (tkproc-port tkproc)))
+      (if port
+          (begin
+
+            (close-port port)
+
+            (let ((status (process-status port)))
+              (if (not (= status 0))
+                  (pp (list 'status status))))
+
+            (tkproc-port-set! tkproc #f))))))
+
+(define tk-start-event-loop
+  (lambda (tkproc)
+    (thread-start!
+     (make-thread
+      (lambda ()
+        (tk-event-loop tkproc))))))
+
+(define tk-event-loop
+  (lambda (tkproc)
+    (let loop1 ()
+      (let ((port (tkproc-port tkproc)))
+        (if (and port
+                 (with-exception-catcher
+                  (lambda (e) #f)
+                  (lambda () (char-ready? port))))
+            (let ((line (read-line port)))
+              ((tkproc-handle tkproc) tkproc line)
+              (loop1))
+            (let loop2 ()
+              (let ((sync-port (tkproc-sync-port tkproc)))
+                (input-port-timeout-set! sync-port 0.05 (lambda () #f))
+                (let ((mut (read sync-port)))
+                  (if (eof-object? mut)
+                      (begin
+                        ((tkproc-handle tkproc) tkproc #f)
+                        (loop1))
+                      (let ((result ((mutex-specific mut))))
+                        (mutex-specific-set! mut result)
+                        (mutex-unlock! mut)
+                        (loop2)))))))))))
+
+(define tk-cmd
+  (lambda (tkproc command)
+    (let ((port (tkproc-port tkproc)))
+      (if port
+          (with-exception-catcher
+           (lambda (e) #f)
+           (lambda ()
+             (display command port)
+             (newline port)
+             (force-output port)))))))
+
+(define tk-cmd-with-result
+  (lambda (tkproc command)
+    (tk-cmd tkproc command)
+    (let ((port (tkproc-port tkproc)))
+      (if port
+          (let loop ()
+            (let ((line (read-line port)))
+              (if (and (string? line)
+                       (>= (string-length line) 1)
+                       (char=? (string-ref line 0) #\!))
+                  (loop)
+                  line)))
+          #f))))
+
+(define refresh
+  (lambda (tkproc segments)
+    (let ((sizes
+           (with-input-from-string
+            (tk-cmd-with-result
+             tkproc
+             "puts \"([winfo width .canv] . [winfo height .canv])\"")
+            read)))
+      (if (and (pair? sizes)
+               (or (not (equal? sizes (tkproc-sizes tkproc)))
+                   (not (equal? segments (tkproc-segments tkproc)))))
+          (begin
+            (tkproc-sizes-set! tkproc sizes)
+            (tkproc-segments-set! tkproc segments)
+            (let* ((width
+                    (car sizes))
+                   (height
+                    (cdr sizes))
+                   (ox
+                    (quotient width 2))
+                   (oy
+                    (quotient height 2))
+                   (sx
+                    (- ox 50))
+                   (sy
+                    (- oy 50)))
+              (redraw tkproc segments sx ox (- sy) oy)))))))
+
+(define redraw
+  (lambda (tkproc segments sx ox sy oy)
+
+    (define transform
+      (lambda (x s o)
+        (let* ((n (inexact->exact (round (* 1000 (+ (* s (real-part x)) o)))))
+               (a (abs n))
+               (s (string-append
+                   (number->string (quotient a 1000))
+                   "."
+                   (substring (number->string (+ 1000 (modulo a 1000))) 1 4))))
+          (if (< n 0) (string-append "-" s) s))))
+
+    (define create-line
+      (lambda (x1 y1 x2 y2 color)
+        (let ((cmd
+               (string-append
+                ".canv create line "
+                (transform x1 sx ox)
+                " "
+                (transform y1 sy oy)
+                " "
+                (transform x2 sx ox)
+                " "
+                (transform y2 sy oy))))
+          (tk-cmd
+           tkproc
+           (if color
+               (string-append cmd " -fill " color)
+               (string-append cmd))))))
+
+    (tk-cmd
+     tkproc
+     ".canv delete all")
+
+    (tk-cmd
+     tkproc
+     (string-append
+      ".canv create text "
+      (transform -1 sx ox)
+      " "
+      (transform -1 sy (+ oy 9))
+      " -text \"(-1,-1)\""))
+
+    (tk-cmd
+     tkproc
+     (string-append
+      ".canv create text "
+      (transform 1 sx ox)
+      " "
+      (transform 1 sy (- oy 9))
+      " -text \"(1,1)\""))
+
+    (create-line -1 -1 -1 1 grid-color)
+    (create-line -1 -1 1 -1 grid-color)
+    (create-line -1 1 1 1 grid-color)
+    (create-line 1 -1 1 1 grid-color)
+    (create-line -1 0 1 0 grid-color)
+    (create-line 0 -1 0 1 grid-color)
+
+    (let loop ((lst segments))
+      (if (pair? lst)
+          (let ((s (car lst)))
+            (let ((start (segm-depart s))
+                  (end (segm-arrivee s)))
+              (let ((start-x (vect-x start))
+                    (start-y (vect-y start))
+                    (end-x (vect-x end))
+                    (end-y (vect-y end)))
+                (create-line start-x start-y end-x end-y #f)))
+            (loop (cdr lst)))))))
+
+(define gui-setup
+  (lambda ()
+
+    ;; La fenetre contient un titre et un canevas :
+    ;;
+    ;;  +------------------------------------------------------+
+    ;;  |                       TP2                            |
+    ;;  +------------------------------------------------------+
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  |                      canevas                         |
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  |                                                      |
+    ;;  +------------------------------------------------------+
+
+    (define handle
+      (lambda (tkproc line)
+        (cond ((eof-object? line)
+               (exit))
+
+              ((eq? line #f)
+               (refresh tkproc segments)))))
+
+    (let ((tkproc (tk-open handle)))
+
+      (tk-cmd tkproc "package require Tk")
+
+      ;; Creer la fenetre.
+
+      (tk-cmd tkproc "wm title . \"TP2\"")
+      (tk-cmd tkproc (string-append "wm geometry . " (number->string (+ canv-width 6)) "x" (number->string (+ canv-height 36))))
+
+      (tk-cmd tkproc (string-append "canvas .canv -width " (number->string canv-width) " -height " (number->string canv-height)))
+
+      (tk-cmd tkproc "pack .canv -side top -expand true -fill both"))))
+
+;;-----------------------------------------------------------------------------
+
+;; Demarrer le GUI.
+
+(gui-setup)
+
+;;-----------------------------------------------------------------------------
---
+++ tp2.scm
@@ -0,0 +1,245 @@
+;; Fichier : "tp2.scm"
+
+;; Auteur(s) : Benoit Hamel, Trong-Tan Nguyen
+
+;;cree une ligne entre deux vecteurs. le segmens cree est un ensemble de petits segments
+(define ligne
+  (lambda (depart arrivee)
+    (lambda (x)
+      (if (procedure? arrivee)
+	  (if (null? (arrivee 0))
+	      depart
+	      (if (list? (arrivee 0))
+		  (if (list? (segm-depart (arrivee 0)))
+		      (append ((ligne depart (segm-depart (segm-depart (arrivee 0)))) x)
+			      (arrivee 0))
+		      (append ((ligne depart (segm-depart (arrivee 0))) x)
+			      (arrivee 0)))
+		  (let ((ligne-x (- (vect-x depart) (vect-x (arrivee 0))))
+			(ligne-y (- (vect-y depart) (vect-y (arrivee 0)))))
+		    (if (< (sqrt (+ (* ligne-x ligne-x) (* ligne-y ligne-y))) (/ 1 10))
+			(list (segm depart (arrivee 0)))
+			(let ((milieu-x (/ (+ (vect-x depart) (vect-x (arrivee 0))) 2))
+			      (milieu-y (/ (+ (vect-y depart) (vect-y (arrivee 0))) 2)))
+			  (let ((milieu (vect milieu-x milieu-y)))
+			    (append ((ligne depart milieu) x)
+				    ((ligne milieu (arrivee 0)) x))))))))
+	  (if (null? arrivee)
+	      depart
+	      (if (list? arrivee)
+		  (if (list? (segm-depart arrivee))
+		      (append ((ligne depart (segm-depart (segm-depart arrivee))) x)
+			      arrivee)
+		      (append ((ligne depart (segm-depart arrivee)) x)
+			      arrivee))
+		  (let ((ligne-x (- (vect-x depart) (vect-x arrivee)))
+			(ligne-y (- (vect-y depart) (vect-y arrivee))))
+		    (if (< (sqrt (+ (* ligne-x ligne-x) (* ligne-y ligne-y))) (/ 1 10))
+			(list (segm depart arrivee))
+			(let ((milieu-x (/ (+ (vect-x depart) (vect-x arrivee)) 2))
+			      (milieu-y (/ (+ (vect-y depart) (vect-y arrivee)) 2)))
+			  (let ((milieu (vect milieu-x milieu-y)))
+			    (append ((ligne depart milieu) x)
+				    ((ligne milieu arrivee) x))))))))))))
+
+;;cree une liste de segments a partir d'une liste de vecteurs
+(define parcours->dessinateur
+  (lambda (parcours)
+	
+	;;si la liste est nulle, retourner le  dessinateur vide
+    (if (null? parcours)
+	(ligne (vect 0 0) (vect 0 0))
+	(foldr ligne '() parcours))))
+
+(define ell
+  (parcours->dessinateur
+   (list (vect -1/2 1) (vect -1/2 -1) (vect 1/2 -1))))
+
+(define vide (parcours->dessinateur '()))
+
+(define triangle
+  (parcours->dessinateur
+   (list (vect -1 -1) (vect 0 1) (vect 1 -1) (vect -1 -1))))
+
+;;vecteurs necessaires pour dessiner les chiffres
+(define zero
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 0)))
+
+(define un
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 1)))
+
+(define deux
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 2)))
+
+(define trois
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 3)))
+
+(define quatre
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 4)))
+
+(define cinq
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 5)))
+
+(define six
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 6)))
+
+(define sept
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 7)))
+
+(define huit
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 8)))
+
+(define neuf
+  (parcours->dessinateur (vector-ref parcours-pour-chiffres 9)))
+
+;;tourne le dessin dans le sens horaire
+(define rotation 
+  (lambda (theta dessinateur)
+    (lambda (x)
+      (map
+       (lambda (i)
+		;;converti le theta (degree) en radians pour les calculs
+		(let ((theta2 (/ (* theta (* 2 3.1416)) 360)))
+	 (segm
+	  (vect	(+ (* (vect-x (segm-depart i))(cos theta2)) (* (vect-y (segm-depart i))(sin theta2)))
+		(- (* (vect-y (segm-depart i))(cos theta2)) (* (vect-x (segm-depart i))(sin theta2))))
+	  (vect	(+ (* (vect-x (segm-arrivee i))(cos theta2)) (* (vect-y (segm-arrivee i))(sin theta2)))
+		(- (* (vect-y (segm-arrivee i))(cos theta2)) (* (vect-x (segm-arrivee i))(sin theta2)))))))
+       (dessinateur 0)))))
+
+;;deplace le dessin
+;;il suffit de changer les vecteurs de tous les segments
+(define translation 
+  (lambda (deplacement-x deplacement-y dessinateur)
+    (lambda (x)
+      (map
+       (lambda (i)
+	 (segm
+	  (vect	(+ (vect-x (segm-depart i)) deplacement-x)
+		(+ (vect-y (segm-depart i)) deplacement-y))
+	  (vect	(+ (vect-x (segm-arrivee i)) deplacement-x)
+		(+ (vect-y (segm-arrivee i)) deplacement-y))))
+       (dessinateur 0)))))
+
+;;change la taille du dessin
+;;il suffit de changer les vecteurs de tous les segments
+(define reduction
+  (lambda (facteur-x facteur-y dessinateur)
+    (lambda (x)
+      (map
+       (lambda (i)
+	 (segm
+	  (vect	(* (vect-x (segm-depart i)) facteur-x)
+		(* (vect-y (segm-depart i)) facteur-y))
+	  (vect	(* (vect-x (segm-arrivee i)) facteur-x)
+		(* (vect-y (segm-arrivee i)) facteur-y))))
+       (dessinateur 0))))) 
+
+;;donne un effet de loupe au dessin
+(define loupe
+  (lambda (facteur dessinateur)
+	(lambda (x)
+	  (map
+	    (lambda (i)
+		;;la valeur de 'm' varie selon le vecteur. m-depart est le 'm' pour le vecteur-depart du segment et m-arrivee pour le 'm' du vecteur arrivee
+		;;nous appliquons simplement la formule
+		  (let ((m-depart (/ (+ 1 facteur)
+							 (+ 1 (* facteur (+ (* (vect-x (segm-depart i)) (vect-x (segm-depart i)))(* (vect-y (segm-depart i)) (vect-y (segm-depart i))))))))
+				(m-arrivee (/ (+ 1 facteur)
+							  (+ 1 (* facteur (+ (* (vect-x (segm-arrivee i)) (vect-x (segm-arrivee i)))(* (vect-y (segm-arrivee i)) (vect-y (segm-arrivee i)))))))))
+				;;avec les m calcules, il suffit de modifie les vecteurs des segments
+				(segm
+					(vect (* (vect-x (segm-depart i)) m-depart)
+						  (* (vect-y (segm-depart i)) m-depart))
+					(vect (* (vect-x (segm-arrivee i)) m-arrivee)
+						  (* (vect-y (segm-arrivee i)) m-arrivee)))))
+		(dessinateur 0)))))
+
+;;met deux dessins ensemble
+(define superposition
+  (lambda (dessinateur1 dessinateur2)
+    (lambda (x)
+      (append (dessinateur1 0) (dessinateur2 0)))))
+
+;;met un dessin sur l'autre
+(define pile
+  (lambda (prop dessinateur1 dessinateur2)
+    (lambda (x)
+		;;mofidifie la taille des dessins pour respecte la proportion
+		;;et deplacer le 2em dessin sur le haut du premier
+      ((superposition (translation 0 (- prop 1) (reduction 1 prop dessinateur1))
+		      (translation 0 prop (reduction 1 (- 1 prop) dessinateur2))) 0))))
+
+;;met deux dessins cote-a-cote en respectant la proportion donnee
+(define cote-a-cote
+  (lambda (prop dessinateur1 dessinateur2)
+    (lambda (x)
+		;;mofidifie la taille des dessins pour respecte la proportion
+		;;et deplacer le 2em dessin a droite du premier
+      ((superposition (translation (- prop 1) 0 (reduction prop 1 dessinateur1))
+		      (translation prop 0 (reduction (- 1 prop) 1 dessinateur2))) 0))))
+
+;;dessine un entier donne en entree
+(define entier->dessinateur
+  (lambda (n)
+    (lambda (x)
+		;;si le nombre est un chiffre, dessiner avec les fonctions pour dessiner un chiffre
+      (if (< n 10)
+	  ((parcours->dessinateur (vector-ref parcours-pour-chiffres n)) 0)
+	  ;;sinon, calculer l'entier le plus a droite du nombre
+	  (let ((entier (modulo n 10))
+		;;garder ce qui n'est pas l'entier
+		(reste (/ (- n (modulo n 10)) 10)))
+	    ((cote-a-cote (- 1 (/ 1 (ceiling (/ (log n) (log 10)))))
+			;;dessiner le reste a gauche
+			  (entier->dessinateur reste)
+			  ;;dessiner l'entier a droite
+			  (parcours->dessinateur (vector-ref parcours-pour-chiffres entier))) 0))))))
+
+;;dessine un arbre a partie d'une liste
+(define arbre->dessinateur
+  (lambda (n)
+    (lambda (x)
+	  ;;cette fonction calcule la profondeur d'une liste (ie le nombre de listes imbriquees)
+      (define depth 
+	(lambda (y)
+	  (if (or (null? y)
+		  (and (not (list? y))
+		       (not (pair? y))))
+	      0
+	      (max (+ 1 (depth (cdr y)))))))
+	   ;;si la liste a une profondeur de 1
+      (if (eq? 1 (depth n)) 
+	  ;;on imprime seulement un arbre de hauteur 1
+	  ((superposition (ligne (vect -1/2 -1) (vect 0 1)) 
+			  (ligne (vect 0 1) (vect 1/2 -1))) 0)
+	  ;;si la liste a une profondeur plus grande que 1
+	  (if (and (or (list? n) 
+		       (pair? n))
+		   (not (null? n)))
+		  ;;si la profondeur des deux sous-arbres est egale
+	      (if (eq? (depth (car n)) (depth (cdr n))) 
+		  ((pile
+		    (- 1 (/ 1 (depth n)))
+			;;on imprime deux sous-arbres avec des appels recursifs
+		    (cote-a-cote 1/2 (arbre->dessinateur (car n)) (arbre->dessinateur (cdr n))) 
+			;;et on imprime un arbre de hauteur 1
+		    (superposition (reduction 1/2 1 (ligne (vect -1 -1) (vect 0 1))) 
+				   (reduction 1/2 1 (ligne (vect 0 1) (vect 1 -1))))) 0)
+		  ;;si les deux sous-arbres n'ont pas la meme profondeur
+		  (if (and (not (eq? 0 (depth (car n)))) 
+			   (not (eq? 0 (depth (cdr n)))))
+		      ((pile (- 1 (/ 1 (depth n)))
+			     (cote-a-cote (/ 1 (depth n))
+					  ;;on imprime le dessinateur vide sous le sous-arbre dont la profondeur est la moins elevee, pour eviter que ce sous-arbre prenne autant de place verticalement que l'autre sous-arbre de profondeur plus grande
+					  (pile (/ (- (depth (cdr n)) (depth (car n))) (depth (cdr n))) vide (arbre->dessinateur (car n))) 
+					  (arbre->dessinateur (cdr n)))
+			     ;;et on imrpime un arbre de hauteur 1
+			     (superposition (reduction (- 1 (/ 1 (depth n))) 1 (ligne (vect -1 -1) (vect 0 1))) 
+					    (reduction (/ 1 (depth n)) 1 (ligne (vect 0 1) (vect 1 -1))))) 0)
+		      ((pile (- 1 (/ 1 (depth n)))
+			     (cote-a-cote (/ 1 (depth n)) (arbre->dessinateur (car n)) (arbre->dessinateur (cdr n)))
+			     (superposition (reduction (- 1 (/ 1 (depth n))) 1 (ligne (vect -1 -1) (vect 0 1)))
+					    (reduction (/ 1 (depth n)) 1 (ligne (vect 0 1) (vect 1 -1))))) 0)))
+		  ;;si la liste ne repond a aucune de des conditions precedentes, on imprime tout simplement le dessinateur vide
+	      ((ligne '() '()) 0)))))) 
